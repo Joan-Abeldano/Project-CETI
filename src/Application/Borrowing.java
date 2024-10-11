19f551bc89f368d37d0545602876aa2aded375d5
labso20 <labso20@labso20-ROG-STRIX-G10CE-G10CE>
2024-10-11 09:24:26 -0600

    Borrowing,Person classes getter/setters

--- src/Application/Borrowing.java
+++ src/Application/Borrowing.java
@@ -12,4 +12,30 @@ public class Borrowing {
     private Item itemBorrowed;
     private String observation;
     private Person person;
+
+    public Item getItemBorrowed() {
+        return itemBorrowed;
+    }
+
+    public void setItemBorrowed(Item itemBorrowed) {
+        this.itemBorrowed = itemBorrowed;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+    
+    
 }
--- src/Application/Person.java
+++ src/Application/Person.java
@@ -15,4 +15,38 @@ public class Person {
     private String group;
     private Date startDate;
     private Date endDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+    
+    
 }
