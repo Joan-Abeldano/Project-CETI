7c8ab3dd9edaef3e70b663fe0e55e5160f683be7
labso20 <labso20@labso20-ROG-STRIX-G10CE-G10CE>
2024-10-17 09:53:39 -0600

    feat: Inventory class def

---
+++ src/Application/Inventory.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Application;
+
+import java.util.List;
+
+/**
+ *
+ * @author labso20
+ */
+public class Inventory {
+    private List<Item> items;
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+    
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    
+    public void deleteItem(Item item) {
+        items.remove(item);
+    }
+}
