7dd1c1edfeb7c1eff2e69ad892fe479cb98c83d2
labso20 <labso20@labso20-ROG-STRIX-G10CE-G10CE>
2024-10-21 17:28:04 -0600

    feat: BorrowingsDBController class

--- src/Application/BorrowingsDBController.java
+++ src/Application/BorrowingsDBController.java
@@ -4,10 +4,24 @@
  */
 package Application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
+
 /**
  *
  * @author labso20
  */
 public class BorrowingsDBController {
-    
+    private final String url = "jdbc:postgresql://localhost:5432/CETI";
+    private final String user = "postgres";
+    private final String password = "Joan";
+
+    public Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
